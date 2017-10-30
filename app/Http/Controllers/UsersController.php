67cacdd65eb25ffb8d3a901daf8b8f12081ae5ce
tester <erta.6224@gmail.com>
2017-10-30 05:17:49 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(1);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -147,6 +147,9 @@ return [
         
         // Add Laravelcollective
         Collective\Html\HtmlServiceProvider::class,
+        
+        // 追加
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
     ],
 
@@ -198,6 +201,8 @@ return [
         
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        // 追加
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
