5366ff01725b5f9dacb0223be41ebaa04d68a986
tester <erta.6224@gmail.com>
2017-10-31 07:18:28 +0000

    follow, unfollow

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -20,6 +20,7 @@ class WelcomeController extends Controller
         if (\Auth::check()) {
             $user = \Auth::user();
             $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            // $microposts = $user->microposts()->orderBy('created_at', 'desc')->get();
 
             $data = [
                 'user' => $user,
