6483067662bdab35c189cd4bc202b7b2f7e50365
tester <erta.6224@gmail.com>
2017-10-31 07:26:23 +0000

    timeline

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -19,9 +19,8 @@ class WelcomeController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-            // $microposts = $user->microposts()->orderBy('created_at', 'desc')->get();
-
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
